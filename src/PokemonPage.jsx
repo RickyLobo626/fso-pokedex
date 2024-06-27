b6020ce2fb38ae01c639579df8dd8c13859207e3
Ricardo Lobo <ricardolobo96@gmail.com>
2024-06-27 22:38:13 +0100

    Fix test errors

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -18,6 +18,7 @@ const PokemonPage = ({ previous, next }) => {
   if (isLoading) {
     return <LoadingSpinner />
   }
+
   if (error) {
     return <ErrorMessage error={error} />
   }
@@ -39,7 +40,7 @@ const PokemonPage = ({ previous, next }) => {
       <div className="links">
         {previous && <Link to={`/pokemon/${previous.name}`}>Previous</Link>}
         <Link to="/">Home</Link>
-        {next && <Link to={`/pokemon/${previous.name}`}>Next</Link>}
+        {next && <Link to={`/pokemon/${next.name}`}>Next</Link>}
       </div>
       <div className={`pokemon-page pokemon-type-${type.name}`}>
         <div
