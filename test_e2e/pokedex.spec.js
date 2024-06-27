79c6b30ecfd79f305c9ce62dc28b7743ef933575
Ricardo Lobo <ricardolobo96@gmail.com>
2024-06-28 00:24:52 +0100

    Add new e2e test 'pokemon details can be opened'

--- test_e2e/pokedex.spec.js
+++ test_e2e/pokedex.spec.js
@@ -1,13 +1,22 @@
-const { test, describe, expect } = require('@playwright/test')
+const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 describe('Pokedex', () => {
-  test('front page can be opened', async ({ page }) => {
+  beforeEach(async ({ page }) => {
     await page.goto('')
+  })
+
+  test('front page can be opened', async ({ page }) => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(
       page.getByText(
         'Pokémon and Pokémon character names are trademarks of Nintendo.'
       )
     ).toBeVisible()
+  })
+
+  test('pokemon details can be opened', async ({ page }) => {
+    await page.getByRole('link', { name: 'ivysaur' }).click()
+    await expect(page).toHaveURL('/pokemon/ivysaur')
+    await expect(page.getByText('chlorophyll')).toBeVisible()
   })
 })
